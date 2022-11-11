75a91f26b0b5fbec3c263e2f1be43926d7e63026
Ekaterina Sedykh <katherine.sedykh23@gmail.com>
2022-11-11 20:36:05 +0100

    first milestone without data

---
+++ script.py
@@ -0,0 +1,36 @@
+def convert_dna_to_rna(dna_seq) -> str:
+    """ Convert DNA sequence to RNA """
+    # T replaced by U 
+    rna_seq = dna_seq.replace("T", "U")
+    return rna_seq
+
+def convert_rna_to_protein(rna_seq) -> str:
+    """ Convert RNA sequence to protein """
+
+    codons_dict = {
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                 
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+    }
+
+    protein = ""
+    for i in range(0, len(rna_seq), 3):
+        codon = rna_seq[i:i+3]
+        #checking for stop codon
+        #if codons_dict[codon] == "Stop":
+        #    break
+        protein+=codons_dict[codon]
+    return protein
